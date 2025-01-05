9364bc05fa32aeb2f7753e66ae6d9589173ebe0e
Norman Douglas <smokewulf@yahoo.com>
2025-01-05 00:48:37 +0000

    commit

--- api.py
+++ api.py
@@ -1,9 +1,21 @@
 
 from fastapi import FastAPI
+from transformers import AutoModel
 
 app = FastAPI()
+
+model = None
+
+@app.on_event("startup")
+async def load_model():
+    global model
+    model = AutoModel.from_pretrained("JeffreyXiang/TRELLIS-image-large")
 
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+@app.get("/model-loaded")
+async def is_model_loaded():
+    return {"model_loaded": model is not None}
 
